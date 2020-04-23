84318c155b9c274c1303e940faed3148a1663211
R A Van Epps <rave@ravanepps.com>
2020-04-23 10:25:35 -0600

    Return null for resource tracker save fn

--- src/block/resource-tracker/save.js
+++ src/block/resource-tracker/save.js
@@ -1,23 +1,9 @@
 /**
  * SAVE: Resource Counter Block
  */
-import { RichText } from '@wordpress/block-editor';
 
 const Save = ( props ) => {
-	const {
-		attributes: {
-			content,
-		},
-		className,
-	} = props;
-
-	return (
-		<RichText.Content
-			className={ className }
-			tagName="p"
-			value={ content }
-		/>
-	);
+	return null;
 };
 
 export default Save;
