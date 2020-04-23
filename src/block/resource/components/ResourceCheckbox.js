c9ee43f3c0540c1b30c1d481e1a8baff66170535
R A Van Epps <rave@ravanepps.com>
2020-04-23 11:10:59 -0600

    Create checkbox component for resource pool

---
+++ src/block/resource/components/ResourceCheckbox.js
@@ -0,0 +1,46 @@
+/**
+ * Display resource uses checkbox.
+ */
+import { CheckboxControl } from '@wordpress/components';
+import { useState, useEffect } from '@wordpress/element';
+
+/**
+ * Handle render of resource pool checkbox.
+ *
+ * @author R A Van Epps <rave@ravanepps.com>
+ * @since  1.0.0
+ *
+ * @param  {Object} props Component props.
+ * @return {ReactElement} Component render JSX.
+ */
+const ResourceCheckbox = ( props ) => {
+	const {
+		disabled,
+		checked,
+		onUpdateResource,
+	} = props;
+	let {
+		used,
+	} = props;
+
+	const [ isChecked, setChecked ] = useState( false );
+
+	// Set initial checked state from props.
+	useEffect( () => {
+		setChecked( checked );
+	} );
+
+	return (
+		<CheckboxControl
+			checked={ isChecked }
+			onChange={ () => {
+				setChecked( ! isChecked );
+				used = ! isChecked ? used + 1 : used - 1;
+				onUpdateResource( 'used', used );
+			} }
+			disabled={ disabled }
+		/>
+	);
+};
+
+export default ResourceCheckbox;
